2446c1f5b759845cbd65a2a3c6cd80786b0e14e0
diveshjoshi0766 <diveshjoshi0766@gmail.com>
2021-08-17 17:55:49 +0530

    final commit

--- src/components/options/Options.js
+++ src/components/options/Options.js
@@ -1,3 +1,5 @@
+
+
 import { joinChannel } from "../../services/agora/video/AgoraVideoService";
 import { muteAudioLocalClient, muteVideoLocalClient, leaveCall } from "../../services/agora/video/AgoraVideoService";
 import "./Options.module.css";
@@ -9,12 +11,18 @@ import {
   faVolumeMute,
   faVideoSlash,
   faGripVertical,
+  faComment,
+  faUserPlus,
 } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { joinChannelForScreenShare, stopScreenShare, startScreenShare} from "../../services/agora/screen-share/AgoraScreenShareService";
 import Chat from "../Chat/Messenger";
 import { initChat, sendActionMessageToChannel } from "../../services/agora/chat/AgoraChatService";
 import { useEffect, useState } from "react";
+import AudioOff from '../../assests/AudioOff.png'
+import Group from '../../assests/Group.png'
+import spreadsheet from '../../assests/spreadsheet.png'
+import Vector from '../../assests/Vector.png'
 
 const Options = () => {
 
@@ -51,6 +59,9 @@ const [screenShare, setScreenShare] = useState(false)
   function shareScreen(){
     startScreenShare();
     setScreenShare(!screenShare)
+    if(!screenShare){
+      // document.getElementsByClassName("screen-share-here").style.display = "none"
+    }
   }
 
 //exit video conference
@@ -63,7 +74,13 @@ function mute_user() {
   sendActionMessageToChannel({type: "MUTE_USER", id: `${localStorage.getItem("2")}`})
 }
 
-function camera_off_user() {
+const [showChatBox, setShowChatBox] = useState(false)
+
+function chatBox(){
+  setShowChatBox(!showChatBox);
+}
+
+function mute_video() {
   sendActionMessageToChannel({type: "CAMERA_OFF_USER", id: `${localStorage.getItem("2")}`})
   sendActionMessageToChannel({type: "CAMERA_OFF_USER", id: `${localStorage.getItem("3")}`})
   sendActionMessageToChannel({type: "CAMERA_OFF_USER", id: `${localStorage.getItem("4")}`})
@@ -80,77 +97,99 @@ function camera_off_user() {
     <>
       <div className="container wrapper">
 {/*Main Video Screen*/}
+
         <div className="main-container">
           <div className="row my-4">
-              <div className="col-sm-4">
+              <div className="col-sm-12">
               <div className="sizing">
-                <div id="local_stream" className="local_stream" style={{ width: "100%", height: "38vh", display: "flex"}}></div>
+                <div id="local_stream" className="local_stream" style={{ width: "100%", height: "90vh", display: "flex"}}></div>
                 </div>
-              </div>
-              <div className="col-sm-4">
-              <div className="sizing" style={{height: "38vh"}}>
-                <div id="remote_video_" style={{ margin:'0px' ,width: "100%", height: "38vh", display: "flex"}}>
-                </div>
-              </div>
               </div>
           </div>
 
-          <div className="row" style={{height: "38vh"}}>
+          {/* <div className="row" style={{height: "38vh"}}>
               <div className="col-sm-4"  style={{height: "fit-content"}}>
-              <div className="sizing" style={{height: "38vh"}}>
-                <div id="screen-share" className="screen-share" style={{ width: "100%", height: "38vh", display: "flex"}}></div>
+                <div className="sizing" style={{height: "38vh"}}>
+                  <div id="screen-share" className="screen-share" style={{ width: "100%", height: "38vh", display: "flex"}}></div>
                 </div>
-              </div>
-              <div className="col-sm-4"  style={{height: "fit-content"}}>
-              <div className="sizing" style={{height: "38vh"}}>
-                <div
-                    id="remote_video_"
-                    style={{ margin:'0px' ,width: "100%", height: "38vh", display: "flex"}}
-                >
-                </div>
-              </div>
-              </div>
-              <div className="col-sm-4"  style={{height: "fit-content"}}>
-              <div className="sizing" style={{height: "38vh"}}>
-                <div id="remote_video_" className="remote_video_" style={{ width: "100%", height: "38vh", display: "flex"}}></div>
-                </div>
-              </div>
-          </div>
+              </div>  
+          </div> */}
         </div>
 
 
 {/*Footer*/}
-
-        <div className="meeting-footer" style={{marginTop: "6vh"}}>
+      
+        
         <div
-            className={"meeting-icons active" + (audio === false ? "" : "active")}>
+            // className={"meeting-icons active fa-2x mic-button" + (audio === false ? "" : "active")}>
+            className={`fa-2x mic-button ${audio ? "background-color" : ""}`}>
             <FontAwesomeIcon
+              className="button-mic"
               icon={faMicrophone}
               onClick={muteAudio}
             />
           </div>
           <div
-            className={"meeting-icons active" + (video === false ? "" : "active")}>
+            className={`fa-lg video-button ${video ? "background-color" : ""}`}>
             <FontAwesomeIcon 
+            className="button-video"
             icon={faVideo} 
             onClick={muteVideo}
             />
           </div>
           <div
-            className={"meeting-icons active off-screenShare" + (screenShare === false ? "" : "active")} id="screenSharing">
+            className={`fa-lg screen-share ${screenShare ? "background-screen-share" : ""}`} id="screenSharing">
             <FontAwesomeIcon 
+            className="button-screen-share"
             icon={faDesktop} 
             onClick={shareScreen}
             />
           </div>
           <div
-            className={"meeting-icons active"}>
+            className={"fa-lg signout-button"}>
           <FontAwesomeIcon 
+            className="button-signout"
             icon={faSignOutAlt} 
             onClick={exit}
           />
           </div>
+          <div
+            className={`fa-lg chat-button ${showChatBox ? "active-chat-box" : ""}`}>
+            <FontAwesomeIcon 
+            className="button-chat"
+            icon={faComment} 
+            onClick={chatBox}
+            />
+          </div>
 
+          <div
+            className={`fa-lg addUser`}>
+            <FontAwesomeIcon 
+            className="button-addUser"
+            icon={faUserPlus} 
+            />
+          </div>
+
+          <div
+            className={`fa-lg audioOff`} >
+            <img className="audioOff-img" src={AudioOff} alt="hello" style={{padding: "0vh"}}></img>
+          </div>
+         
+          <div
+            className={`fa-lg group`} >
+            <img className="audioOff-img" src={Group} alt="hello" style={{padding: "0vh"}}></img>
+          </div>
+
+          <div
+            className={`fa-lg spreadsheet`} >
+            <img className="spreadsheet-img" src={spreadsheet} alt="hello" style={{padding: "0vh"}}></img>
+          </div>
+
+          <div
+            className={`fa-lg Vector`} >
+            <img className="Vector-img" src={Vector} alt="hello" style={{padding: "0vh"}}></img>
+          </div>
+         
 
           {/* <div
             className={"meeting-icons active"}>
@@ -162,16 +201,28 @@ function camera_off_user() {
           <div
             className={"meeting-icons active"}>
           <FontAwesomeIcon 
-            onClick={camera_off_user}
+            onClick={mute_video}
             icon={faVideoSlash} 
           />
           </div> */}
-          <div className="meeting-icons" id="chat-box">
-          <Chat></Chat>
-          </div>
         </div>
 
+        <div className="chat-box-apperence $">
+          {/* <Chat></Chat> */}
+          {showChatBox ? <Chat/> : null}
         </div>
+
+        
+        <div className="" style={{height: "0vh"}}>
+            <div id="remote_video_" style={{ margin:'0px' ,width: "40vh", height: "38vh", display: "flex", position: "absolute", top: "0vh", left: "0"}}>
+            </div>               
+        </div>
+
+          <div className="" style={{height: "0vh"}}>
+            <div id="screen-share-here" className="screen-share-here" style={{ margin:'0px' ,width: "40vh", height: "38vh", display: "flex", position: "absolute", top: "0vh", left: "40vh"}}>
+            </div>               
+          </div>
+
 
     </>
   )
